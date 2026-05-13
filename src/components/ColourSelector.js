e2881a48d2e2f74df429883627e1526453b7bcd9
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2026-05-13 21:04:37 +0530

    Update ColourSelector to handle color selection
    Refactor ColourSelector to use updateSelectedColor and render color buttons.

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,29 @@
+
+
 import React from 'react';
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
-  const { background } = config;
+  const { updateSelectedColor } = props;
+  
+  // Array of colors to render as buttons
+  const colors = ["red", "green", "blue"];
+
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
-    </button>
-  )
-}
+    <div className="selector-container">
+      {colors.map((color) => (
+        <button
+          key={color}
+          // The handler is called with the specific color string
+          onClick={() => updateSelectedColor(color)}
+          // Using inline styles or dynamic classes to identify the button
+          style={{ backgroundColor: color, margin: "5px", padding: "10px" }}
+          data-testid={color}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
+  );
+};
+
 export default ColourSelector;
